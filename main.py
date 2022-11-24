c091eacf046d2796d05db36507d1279375c4ced5
HydroGest <2445691453@qq.com>
2022-11-24 16:05:47 +0800

    基本功能实现

---
+++ main.py
@@ -0,0 +1,77 @@
+'''
+File: main.py
+Author: github.com/HydroGest
+Description: See who are peeping screens.
+'''
+
+from flask import Flask, render_template, request, send_from_directory
+import os,time,json
+app = Flask(__name__)
+
+def SearchObject(groupId,info):
+    fileName='./data/'+groupId+'.json'
+    if os.path.isfile(fileName)==False:
+        createdTime= int(time.time())
+        with open(fileName,"w",encoding="utf-8") as f:
+            f.write('{"createdTime":'+str(createdTime)+',"peeperList":[]}')
+    fileContents=''
+    with open(fileName,'r') as f:
+        fileContents=fileContents+f.read()
+    groupDict = json.loads(fileContents)
+    if int(time.time())-int(groupDict['createdTime'])>300:
+        createdTime=time.time()
+        groupDict={
+            "createdTime":createdTime,
+            "peeperList":[]
+        }
+    if (info in groupDict['peeperList'])==False:
+        groupDict['peeperList'].append(info)
+    with open(fileName,"w",encoding="utf-8") as f:
+        f.write(json.dumps(groupDict))
+    return {
+        'groupId': groupId,
+        'createdTime': groupDict['createdTime'],
+        'peeperList': groupDict['peeperList']
+    }
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+@app.route('/search')
+def search():
+    #groupId = request.form['GroupId']
+    groupId = request.args.get('GroupId', '')
+    ip= request.remote_addr
+    userAgent = request.headers.get("User-Agent")
+    groupInfo = SearchObject(groupId,{
+        'ip':ip,
+        'userAgent': userAgent
+    })
+    return render_template(
+        'group.html',
+        GroupId=groupId,
+        createdTime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(groupInfo['createdTime'])),
+        peeperList=groupInfo['peeperList']
+    )
+
+@app.route('/api/search/')
+def api():
+    groupId = request.form('GroupId', '')
+    ip = request.remote_addr
+    userAgent = request.headers.get("User-Agent")
+    groupInfo = SearchObject(groupId,{
+        'ip':ip,
+        'userAgent': userAgent
+    })
+    return groupInfo
+
+@app.route('/api/getimg/<groupId>/icon.jpg')
+def imgget(groupId):
+    ip = request.remote_addr
+    userAgent = request.headers.get("User-Agent")
+    groupInfo = SearchObject(groupId,{
+        'ip':ip,
+        'userAgent': userAgent
+    })
+    return send_from_directory('','icon128.jpg')
---
+++ rundev.sh
@@ -0,0 +1,3 @@
+. venv/bin/activate
+export FLASK_APP=main
+flask run --port 8080 --host=0.0.0.0
